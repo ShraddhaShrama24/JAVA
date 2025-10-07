7196e09805dd91a81e67a46665c3c373efbd6483
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-07 17:01:07 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Addition
+{
+    public static void main(String args[])
+    {
+        int a,b,add;
+        a=34;
+        b=89;
+        add=a+b;
+        System.out.println("add="+add);
+    }
+}
