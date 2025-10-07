5a629e1fcfae17d0dc858143f9ae9b929b2c7677
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-07 17:01:51 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+class Multiplication
+{
+    public static void main(String args[])
+    {
+        int a,b,multiply;
+        a=56;
+        b=234;
+        multiply=a*b;
+        System.out.println("multiply="+multiply);
+    }
+}
