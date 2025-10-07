402d513dad08fa87dc114f5732f1e41d61773416
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-07 17:03:01 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,11 @@
+class Division
+{
+    public static void main(String args[])
+    {
+        double a,b,divide;
+        a=23;
+        b=45678;
+        divide=a/b;
+        System.out.println("divide="+divide);
+    }
+}
