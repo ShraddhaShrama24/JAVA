965faf14aecbe466fe68806eb94ed175fb6b2b72
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-07 17:02:17 +0530

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,14 @@
+class Subtration
+{
+    public static void main(String args [])
+    {
+        int a,b,sub;
+        {
+        a=23;
+        b=79;
+        sub=a-b;
+        System.out.println("sub="+sub);
+        }
+    }
+    
+}
