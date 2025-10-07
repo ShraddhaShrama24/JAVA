27f1e7eae5ac4ab1c1262d327ba9feb9a524d3a6
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-07 17:07:42 +0530

    Create Subtraction2.java

---
+++ Subtraction2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class SUBTRACT
+{
+    public static void main(String arg[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,sub;
+        System.out.println("Enter the value of a");
+        a=sc.nextInt();
+        System.out.println("Enter the value of b");
+        b=sc.nextInt();
+        sub=a+b;
+        System.out.println("sub="+sub);
+    }
+}
