7d0368f2b482d5ba4491a35bddabe21177480be8
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-07 17:04:24 +0530

    Create Maths.java

---
+++ Maths.java
@@ -0,0 +1,23 @@
+class MATHS
+{
+    public static void main (String args [])
+    {
+        double a,b,add,sub,mul,div;
+        a =27;
+        b=12;
+        add=a+b;
+        System.out.println("add="+add);
+        a=23;
+        b=45;
+        sub=a-b;
+        System.out.println("sub="+sub);
+        a=67;
+        b=86;
+        mul=a*b;
+        System.out.println("mul="+mul);
+        a=12;
+        b=3;
+        div=a/b;
+        System.out.println("div="+div);
+    }
+}
