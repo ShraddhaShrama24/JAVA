262973979de1391ca57e57a16ca624022ac02929
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-08 21:40:59 +0530

    Create Multiplication2.java

---
+++ Multiplication2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Multiply
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,mul;
+        System.out.println("Enter the value of a");
+        a=sc.nextInt();
+        System.out.println("Enter the value of b");
+        b=sc.nextInt();
+        mul=a*b;
+        System.out.println("mul="+mul);
+    }
+}
