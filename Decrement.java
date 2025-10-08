8f6d84316234ea0452c70cc7ab72145f143a2907
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-08 21:41:31 +0530

    Create Decrement.java

---
+++ Decrement.java
@@ -0,0 +1,11 @@
+class DecrementNo
+{
+    public static void main(String args[])
+    {
+        System.out.println("The decrement no. is");
+        for(int i = 50; i > 1;i++)
+        {
+          System.out.println(-i);  
+        }
+    }
+}
