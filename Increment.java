65218ce95b6851798d4410d53356bff264ea75e4
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-08 21:39:55 +0530

    Create Increment.java

---
+++ Increment.java
@@ -0,0 +1,12 @@
+class Increment 
+{
+    public static void main(String args[])
+    {
+        System.out.println("The increment no. are");
+        int i;
+        for(i = 1;i<=100;i++)
+        {
+            System.out.println(+i);
+        }
+    }
+}
