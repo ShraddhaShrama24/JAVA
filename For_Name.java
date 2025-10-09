ef847bd8bfb399868cdfb878c8561176c7bdc88a
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-09 23:19:16 +0530

    Create For_Name.java

---
+++ For_Name.java
@@ -0,0 +1,13 @@
+//Write a program to input a name using a print greeting messeage for example input Shraddha Sharma,output how are you doing
+class ForName
+{
+    public static void main(String args[])
+    {
+        System.out.println("Shraddha Sharma");
+        for(int i=1;i<=1;i++)
+        {
+            System.out.println("How are you doing");
+            System.out.println();
+        }
+    }
+}
