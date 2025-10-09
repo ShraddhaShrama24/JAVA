829d27922724fb330440a4cc5c1bf9ba5d43ed92
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-09 23:20:13 +0530

    Create For_Hooby.java

---
+++ For_Hooby.java
@@ -0,0 +1,13 @@
+//write a program to enter your favourate hobby for example input whatt is your hobby ,output my hobby is spending money
+class For2
+{
+    public static void main(String args[])
+    {
+        System.out.println("What is your hobby?");
+        for(int i=1;i<=1;i++)
+        {
+          System.out.println("My hobby is Spending money.");
+          System.out.println();
+        }
+    }
+}
