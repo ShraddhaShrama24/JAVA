3bb8c213d0de76645dc0c171d13a593a1ea6d94d
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-09 23:19:41 +0530

    Create For_loop.java

---
+++ For_loop.java
@@ -0,0 +1,12 @@
+class For
+{
+    public static void main(String args[])
+    {
+        System.out.println("The given number are");
+        int i;
+        for(i=1;i<=20;i++)
+        {
+            System.out.println(+i);
+        }
+    }
+}
