6b12f85d06742daf0a9c5b29ec01316226b8cec9
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-10 20:42:37 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,41 @@
+
+class Switch
+{
+  public static void main(String args[]) 
+  {
+      int i= Integer.parseInt(args[0]);
+      Swtich(i);
+      {
+          case1:
+            System.out.println("The no. is 1");
+            break;
+          case2:
+            System.out.println("The no. is 2");
+            break;
+          case3:
+            System.out.println("The no. is 3");
+            break;
+          case4:
+            System.out.println("The no. is 4");
+            break;
+          case5:
+            System.out.println("The no. is 5");
+            break;
+          case6:
+            System.out.println("The no. is 6");
+            break;
+          case7:
+            System.out.println("The no. is 7");
+            break;
+          case8:
+            System.out.println("The no. is 8");
+            break;
+          case9:
+            System.out.println("The no. is 9");
+            break;
+          case10:
+            System.out.println("The no.is 10");
+            break;
+      }
+  }
+}
