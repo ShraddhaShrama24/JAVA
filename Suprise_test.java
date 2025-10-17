f73774387f270addffcd8166b90b55e49eec5aa2
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-17 21:16:27 +0530

    Add program to print numbers from 1 to 15
    Implement a program to print numbers from 1 to 15 using a for-loop.

---
+++ Suprise_test.java
@@ -0,0 +1,13 @@
+//Write a programe to print increament from 1 - 15 by for-loop 
+class Surprise_test
+{
+    public static void main(String args[])
+    {
+        System.out.println("The number are");
+        int i;
+        for(i=1;i<=15;i++)
+        {
+            System.out.println(+i);
+        }
+    }
+}
