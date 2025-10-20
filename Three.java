7e215534e190b5f23fd5acaaacad2f8013202e36
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-20 21:47:36 +0530

    Implement volume calculation for a cube

---
+++ Three.java
@@ -0,0 +1,13 @@
+//Write a program to find the volume of cube.
+class Three
+{
+    public static void main(String args[])
+    {
+        int a,b,c,volumeofacube;
+        a=34;
+        b=45;
+        c=56;
+        volumeofacube=a*b*c;
+        System.out.println("volumeofacube="+volumeofacube);
+    }
+}
