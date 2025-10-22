2c47701e4b4e6ae322c499a9ebaadd63d8180b94
Shraddha Sharma <sharddhasharma89603abc@gmail.com>
2025-10-22 19:25:53 +0530

    Add Feelings.java with main method and output

---
+++ Feelings.java
@@ -0,0 +1,10 @@
+class feelings
+{
+    public static void main (String args[])
+    {
+        System.out.println("When we choose java");
+        System.out.println("I am a java programmer :D");
+        System.out.println("After 1 year");
+        System.out.println(" Why tf i choose java i am DONE D:");
+    }
+}
